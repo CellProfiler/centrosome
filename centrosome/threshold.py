8da4dcf0ec8cf9e7680d747ae023a69803a97305
David Stirling <daves1990@hotmail.co.uk>
2019-10-21 15:40:40 -0400

    Remove numpy_histogram
    This function was over 10 years old and appears to be redundant. Replaced numpy_histogram calls with straight np.histogram calls.

--- centrosome/threshold.py
+++ centrosome/threshold.py
@@ -758,8 +758,8 @@ def sum_of_entropies(image, mask, binary_image):
     log_bg = np.log2(bg)
     #
     # Make these into histograms
-    hfg = numpy_histogram(log_fg, 256, range=(lower,upper))[0]
-    hbg = numpy_histogram(log_bg, 256, range=(lower,upper))[0]
+    hfg = np.histogram(log_fg, 256, range=(lower,upper), normed=False, weights=None)[0]
+    hbg = np.histogram(log_bg, 256, range=(lower,upper), normed=False, weights=None)[0]
     #hfg = scipy.ndimage.histogram(log_fg,lower,upper,256)
     #hbg = scipy.ndimage.histogram(log_bg,lower,upper,256)
     #
@@ -810,11 +810,3 @@ def inverse_log_transform(image, d):
     d - object returned by log_transform
     '''
     return np.exp(unstretch(image, d["log_min"], d["log_max"]))
-
-def numpy_histogram(a, bins=10, range=None, normed=False, weights=None):
-    '''A version of numpy.histogram that accounts for numpy's version'''
-    args = inspect.getargs(np.histogram.__code__)[0]
-    if args[-1] == "new":
-        return np.histogram(a, bins, range, normed, weights, new=True)
-    return np.histogram(a, bins, range, normed, weights)
-    
