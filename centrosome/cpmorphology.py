833ca7cf0740cfcc56606ffc33f6132a6195e4fe
bethac07 <bcimini@broadinstitute.org>
2023-07-19 16:22:29 -0400

    numpy deprecations

--- centrosome/cpmorphology.py
+++ centrosome/cpmorphology.py
@@ -902,8 +902,8 @@ def triangle_areas(p1, p2, p3):
     
     p1,p2,p3 - three Nx2 arrays of points
     """
-    v1 = (p2 - p1).astype(np.float)
-    v2 = (p3 - p1).astype(np.float)
+    v1 = (p2 - p1).astype(np.float64)
+    v2 = (p3 - p1).astype(np.float64)
     # Original:
     #   cross1 = v1[:,1] * v2[:,0]
     #   cross2 = v2[:,1] * v1[:,0]
