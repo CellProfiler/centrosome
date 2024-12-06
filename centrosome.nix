2620f4f30f787f57a56dbbdf601f77f1fdd8ad77
Alán F. Muñoz <afer.mg@gmail.com>
2024-12-06 16:45:23 -0500

    fix(nix): use proper sha

--- centrosome.nix
+++ centrosome.nix
@@ -21,12 +21,11 @@ buildPythonPackage {
   src = fetchFromGitHub {
     owner = "afermg";
     repo = "centrosome";
-    rev = "15093d60";
-    sha256 = "";
+    rev = "87f142083e010c88666efed341a905b98a3977c3";
+    sha256 = "sha256-bCbsQ67b67N3+TBOorZJV7URCglHrUj5eLaV648dH/w=";
   };
   pyproject = true;
   buildInputs = [
-    pytest
     cython
     pip
     setuptools
@@ -36,6 +35,7 @@ buildPythonPackage {
     numpy
     scipy
     scikit-image
+    pytest
   ];
   pythonImportsCheck = [];
 
