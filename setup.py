65e12729a296341d4be3316aab4a62eb976e7d8a
Alán F. Muñoz <afer.mg@gmail.com>
2025-10-21 11:54:14 -0400

    meta: add line to support py39

--- setup.py
+++ setup.py
@@ -93,6 +93,7 @@ setuptools.setup(
         "Programming Language :: C++",
         "Programming Language :: Cython",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
