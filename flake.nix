4eff192965db7815755b5823d5eeb8fc63303b81
Alán F. Muñoz <afer.mg@gmail.com>
2025-10-21 11:38:42 -0400

    deps(nix): update 24.05->unstable and python 311->313

--- flake.nix
+++ flake.nix
@@ -1,57 +1,63 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs_master.url = "github:NixOS/nixpkgs/master";
     systems.url = "github:nix-systems/default";
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.systems.follows = "systems";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    systems,
-    ...
-  } @ inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      systems,
+      ...
+    }@inputs:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = import nixpkgs {
           system = system;
           config.allowUnfree = true;
         };
-        
-      in
-        with pkgs; rec {
-          callPackage = lib.callPackageWith (pkgs // packages // python3Packages);
-          
-          packages = {
-              centrosome = callPackage ./nix/centrosome.nix {};
-            };
 
-          devShells = {
-            default = let
-              python_with_pkgs = (pkgs.python3.withPackages(pp: [
-                packages.centrosome
-                ]));
-              in
-              mkShell {
-                packages = [
-                  python_with_pkgs
-                ];
-                venvDir = "./.venv";
-                postVenvCreation = ''
-                  unset SOURCE_DATE_EPOCH
-                '';
-                postShellHook = ''
-                  unset SOURCE_DATE_EPOCH
-                '';
-                shellHook = ''
-                  runHook venvShellHook
-                  export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}:$PYTHONPATH
-                '';
-              };
-          };
-        }
+      in
+      with pkgs;
+      rec {
+        callPackage = lib.callPackageWith (pkgs // packages // python3Packages);
+
+        packages = {
+          centrosome = callPackage ./nix/centrosome.nix { };
+        };
+
+        devShells = {
+          default =
+            let
+              python_with_pkgs = (
+                pkgs.python3.withPackages (pp: [
+                  packages.centrosome
+                ])
+              );
+            in
+            mkShell {
+              packages = [
+                python_with_pkgs
+              ];
+              venvDir = "./.venv";
+              postVenvCreation = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              postShellHook = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              shellHook = ''
+                runHook venvShellHook
+                export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}:$PYTHONPATH
+              '';
+            };
+        };
+      }
     );
 }
