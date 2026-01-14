9caff85e4b77a792d3ffb8cf24afcba6f391b883
Nodar Gogoberidze <gnodar01@gmail.com>
2026-01-14 13:56:55 -0500

    Direct download script to dist dir

--- scripts/download_artifacts.sh
+++ scripts/download_artifacts.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir artifacts
+mkdir "${PIXI_PROJECT_ROOT}"/artifacts
 run_id=$(gh run list --workflow meiosis -L 1 --json databaseId -q ".[0].databaseId")
-gh run download "$run_id"
-mv -vf artifacts/*/* dist/
-rmdir artifacts/*
-rmdir artifacts/
+gh run download "$run_id" --dir "${PIXI_PROJECT_ROOT}"/artifacts
+mv -vf "${PIXI_PROJECT_ROOT}"/artifacts/*/* "${PIXI_PROJECT_ROOT}"/dist/
+rmdir "${PIXI_PROJECT_ROOT}"/artifacts/*
+rmdir "${PIXI_PROJECT_ROOT}"/artifacts/
