5d632591bd056b0971d7e7630b430a0baebbf911
Nodar Gogoberidze <gnodar01@gmail.com>
2026-01-14 12:21:42 -0500

    Force move artifacts

--- scripts/download_artifacts.sh
+++ scripts/download_artifacts.sh
@@ -3,6 +3,6 @@
 mkdir artifacts
 run_id=$(gh run list --workflow meiosis -L 1 --json databaseId -q ".[0].databaseId")
 gh run download "$run_id"
-mv artifacts/*/* dist/
+mv -vf artifacts/*/* dist/
 rmdir artifacts/*
 rmdir artifacts/
